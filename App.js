8323414e9c1c59e5b50b0d7ac226fb3fb24c5f70
musab-mohamed206 <30497165+musab-mohamed206@users.noreply.github.com>
2022-05-18 09:30:22 +0300

    add custom fonts

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import { Asset } from 'expo-asset'
+import AppLoading from 'expo-app-loading';
+
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular' : require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold' : require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(true);
+  getFonts();
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return(
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+
+  }
+
 }
