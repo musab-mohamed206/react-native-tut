284c6d2b021f6e2fbbedb226d23e61cac007ad8a
musab-mohamed206 <30497165+musab-mohamed206@users.noreply.github.com>
2022-05-22 11:03:58 +0300

    passing data between screens

--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, FlatList, TouchableOpacity} from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        { title: 'Zela, Breeath of Freesh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Zela, Breeath of Freesh Air1', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Zela, Breeath of Freesh Air2', rating: 2, body: 'lorem ipsum', key: '3' },
+    ]);
     const pressHandler = () => {
         navigation.navigate('ReviewDetails');
     }
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='go to review dets' onPress={pressHandler}/>
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetsils() {
+export default function ReviewDetsils({navigation}) {
     return(
         <View style={globalStyles.container}>
-            <Text>ReviewDetsils Screen</Text>
+            <Text>{ navigation.getParam('title') }</Text>
+            <Text>{ navigation.getParam('body') }</Text>
+            <Text>{ navigation.getParam('rating') }</Text>
         </View>
     )
 }
